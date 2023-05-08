edccd28f7c8da749ca905183c003b8faa1d1a8bd
stavco9 <stavco9@gmail.com>
2023-05-08 19:26:13 +0300

    Removed helm charts dir

--- terraform/live/stav-devops/aws/us-east-1/kops/cluster/helm-charts/install-fluentd.sh
+++
@@ -1,4 +0,0 @@
-#helm repo add kokuwa https://kokuwaio.github.io/helm-charts
-#helm repo update
-#helm install fluentd kokuwa/fluentd-elasticsearch -n logging --create-namespace -f fluentd-values.yaml
-helm upgrade fluentd kokuwa/fluentd-elasticsearch -n logging -f fluentd-values.yaml
